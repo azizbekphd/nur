c32ec44c91236a7e8b877237b37ba1a923bf81ef
Azizbek_PhD <74199590+azizbekphd@users.noreply.github.com>
2023-11-10 22:10:51 +0500

    patch

--- components/ui/ProfileMenu/index.tsx
+++ components/ui/ProfileMenu/index.tsx
@@ -5,43 +5,50 @@ import useI18n from "../../../i18n";
 import { CleanButton } from "..";
 import { useAuthSession } from "../../../hooks";
 
-type ProfileMenuProps = {}
+type ProfileMenuProps = {};
 
 const ProfileMenu = ({}: ProfileMenuProps) => {
   const { session } = useAuthSession();
   const { S } = useI18n();
-  
+
   return (
-    <div className={ styles.wrapper } tabIndex={1}>
-      <div className={ styles.buttonWrapper }>
-        {/* <Image
-          src={ session!.user!.image! }
-          alt={ session!.user!.name! }
-          width={40} height={40}
-          className={ styles.profileImage }
-          /> */}
-        {/* <h4 className={ styles.name }>
-          { session!.user!.name!.split(" ").map(
-            (w,i,a)=><Fragment key={i.toString()}>
-              <span>
-                {w}
-              </span>
-              {i === a.length - 1 ? "" : " "}
-            </Fragment>
-          ) }
-        </h4> */}
+    <div className={styles.wrapper} tabIndex={1}>
+      <div className={styles.buttonWrapper}>
+        {session?.user?.user_metadata.image ? (
+          <Image
+            src={session!.user!.user_metadata.image!}
+            alt={session!.user!.user_metadata.name!}
+            width={40}
+            height={40}
+            className={styles.profileImage}
+          />
+        ) : (
+          <></>
+        )}
+        <h4 className={styles.name}>
+          {(session!.user!.user_metadata.name! as string)
+            .split(" ")
+            .map((w, i, a) => (
+              <Fragment key={i.toString()}>
+                <span>{w}</span>
+                {i === a.length - 1 ? "" : " "}
+              </Fragment>
+            ))}
+        </h4>
       </div>
-      <ul className={ styles.menuItems }>
+      <ul className={styles.menuItems}>
         <li>
-          <CleanButton onClick={ ()=>{
+          <CleanButton
+            onClick={() => {
               // signOut()
-            } }>
-            { S.signOut }
+            }}
+          >
+            {S.signOut}
           </CleanButton>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileMenu
+export default ProfileMenu;
